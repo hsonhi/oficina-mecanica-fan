ab6eceb0280307abb3a7b432f59a9a114c7699ee
hsonhi <heraldosonhi@gmail.com>
2026-09-06 07:14:05 +0100

    initialize repo

---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,21 @@
+import { usePage } from '@inertiajs/react';
+
+import AppLogoIcon from '@/components/app-logo-icon';
+
+export default function AppLogo() {
+    const { name } = usePage().props;
+
+    return (
+        <>
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            </div>
+            <img src="/logo.png" alt="Logo" className="size-5 fill-current text-white dark:text-black" />
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">
+                   Oficina Mecanica FAN
+                </span>
+            </div>
+        </>
+    );
+}
